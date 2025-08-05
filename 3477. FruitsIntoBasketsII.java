bb956b32a9934652d78a2f1935238e06f2817897
Shruti Khera <shruti.khera.cseaiml.2022@miet.ac.in>
2025-08-06 01:34:25 +0530

    fruits into basket

---
+++ 3477. FruitsIntoBasketsII.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+       boolean[] used = new boolean[fruits.length];
+       int unplaced = 0;
+       for(int i =0;i<fruits.length;i++){
+        boolean placed = false;
+        for(int j = 0; j<baskets.length;j++){
+            if(!used[j] && baskets[j] >= fruits[i]){
+                used[j] = true;
+                placed = true;
+                break;
+            }
+        }
+        if(!placed){
+            unplaced++;
+        }
+       }
+       return unplaced;
+    }
+}
