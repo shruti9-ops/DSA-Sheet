62cd187fa40276c0818116bb36d5de67b1634730
Shruti Khera <shruti.khera.cseaiml.2022@miet.ac.in>
2025-09-02 10:26:10 +0530

    Create 169 Majority Element

---
+++ 169MajorityElement.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        // int count =0;
+        // int major =0;
+        // for(int n : nums){
+        //     if(count ==0){
+        //         major =n;
+        //     }
+        //     count += n==major? 1 : -1;
+        // }
+        // return major;
+        // }
+      // 2nd solution
+        int n = nums.length;
+        Arrays.sort(nums);
+        int s = nums[n/2];
+        return s;
+    }
+}
